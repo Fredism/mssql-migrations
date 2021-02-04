208cbbf54a6121eb7429e8bfe1a4665bf112dfb0
Fred Erlikh <erlikhf@gmail.com>
2021-02-04 02:00:20 -0500

    fix: drop index syntax

--- Query.cs
+++ Query.cs
@@ -168,10 +168,7 @@ namespace Migrate
             return string.Join("\n", new string[]
             {
                 $"IF EXISTS (SELECT * FROM sys.indexes WHERE name = '{index.name}' and object_id = OBJECT_ID('{index.qualified_table_name}'))",
-                "BEGIN",
-                $"\tALTER TABLE {index.qualified_table_name}",
-                $"\tDROP INDEX [{index.name}]",
-                "END\n"
+                $"DROP INDEX [{index.name}] ON {index.qualified_table_name}",
             });
         }
         public static string AddPrimaryKey(SysConstraint key)
